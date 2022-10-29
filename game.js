7fbe4e58f90ee293b18e4e59417e0fdca78402a7
sergiovalbuena <iosergio.valbuena@gmail.com>
2022-10-29 13:04:47 -0700

    array bidimensional para generar el maze

--- game.js
+++ game.js
@@ -1,15 +1,43 @@
 const canvas = document.querySelector('#game');
 const game = canvas.getContext('2d');
 
-window.addEventListener('load', startGame);
+let canvasSize;
+let elementsSize;
 
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-function startGame() {
-  game.fillRect(0,50, 10, 10);
-  //game.clearRect(0, 0, 3, 3);
-  game.font = '25px Verdana';
-  game.fillStyle = 'Purple';
-  game.textAlign = 'center'; //start, end
-  game.fillText('Happy', 100, 100);
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
 }
 
+function startGame() {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  const map = maps[2];
+  const mapRows  = map.trim().split('\n');
+  const mapRowsCols = mapRows.map(row => row.trim().split(''));
+  console.log({ map, mapRows, mapRowsCols });
+  console.log(mapRowsCols[0][1]);
+ 
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 0; col <= 10; col++) {
+      game.fillText(emojis[mapRowsCols[row - 1][col - 1]], elementsSize * col, elementsSize * row);
+    }
+  }
+}
